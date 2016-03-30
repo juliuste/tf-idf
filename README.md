c3e67e275a83709f9394cd709d7d7e2f606eba5f
Julius Tens <mail@juliustens.de>
2016-03-30 06:07:03 +0200

    Initial changes, added German language support, removed NLTK dependencies and English/French language support.

---
+++ README.md
@@ -0,0 +1,19 @@
+# tf-idf
+
+This script implements the TF-IDF term relevance scoring as described on [wikipedia's article](http://en.wikipedia.org/wiki/Tf–idf)
+
+Its purpose is to generate the TF-IDF ratings for a collection of documents *in German*. This script will also tokenize the input files to extract words (removes punctuation).
+
+## Usage
+- Create a file to hold the paths+names of all your documents (in the example shown: `input.txt`)
+- Make sure you have the full paths to the files listed in the file above each on a separate line
+- For now, the documents are only collections of text, no HTML, XML, RDF, or any other format
+- Simply run this script file with your input file as a single parameter, for example:
+```python tfidf.py input.txt```
+- This script will generate new files, one for each of the input files, with the prefix `tfidf_` which contains terms with corresponding tfidf score, each on a separate line
+
+This script is a fork from [Yasser Elsayed](https://github.com/yebrahim/)'s [TF-IDF-Generator](https://github.com/yebrahim/TF-IDF-Generator) module, currently in alpha status and still in active developement. Feel free.
+
+## Contributing
+
+If you found a bug, want to propose a feature or feel the urge to complain about your slightly below-average life, feel free to visit [the issues page](https://github.com/juliuste/tf-idf/issues).
--- tfidf.py
+++ tfidf.py
@@ -3,20 +3,21 @@
 
 """
 File: tfidf.py
-Author: Yasser Ebrahim
-Date: Oct 2012
+Original author: Yasser Ebrahim
+Release date: Oct 2012
+
+Modified by: Julius Tens
+E-Mail: mail@julius-tens.de
+Web: https://github.com/juliuste
+Date: 30.03.2016
 
 Generate the TF-IDF ratings for a collection of documents.
 
 This script will also tokenize the input files to extract words (removes punctuation and puts all in
-    lower case), and it will use the NLTK library to lemmatize words (get rid of stemmings)
-
-IMPORTANT:
-    A REQUIRED library for this script is NLTK, please make sure it's installed along with the wordnet
-    corpus before trying to run this script
+    lower case).
 
 Usage:
-    - Create a file to hold the paths+names of all your documents (in the example shown: input_files.txt)
+    - Create a file to hold the paths+names of all your documents (in the example shown: input.txt)
     - Make sure you have the full paths to the files listed in the file above each on a separate line
     - For now, the documents are only collections of text, no HTML, XML, RDF, or any other format
     - Simply run this script file with your input file as a single parameter, for example:
@@ -26,99 +27,71 @@ Usage:
 
 """
 
+supported_langs     = ('german')
 # a list of (words-freq) pairs for each document
 global_terms_in_doc = {}
 # list to hold occurrences of terms across documents
 global_term_freq    = {}
 num_docs            = 0
-lang        = 'english'
+lang                = 'german'
 lang_dictionary     = {}
 top_k               = -1
-supported_langs     = ('english', 'french')
+lemmaPath           = 'german/lemmata/list.csv'
+stopwordPath        = 'german/stopwords/list.txt'
 
-# support for custom language if needed
-def loadLanguageLemmas(filePath):
-    print('loading language from file: ' + filePath)
-    f = open(filePath)
+def loadLemmata(filePath): # updated
+    f = open(filePath, 'r')
     for line in f:
+        if len(line) == 0 or line[0] == '#':
+            continue
         words = line.split()
-        if words[1] == '=' or words[0] == words[1]:
+        if len(words) != 2 or words[0] == words[1]:
             continue
         lang_dictionary[words[0]] = words[1]
 
-def remove_diacritic(words):
-    for i in range(len(words)):
-        w = unicode(words[i], 'ISO-8859-1')
-        w = unicodedata.normalize('NFKD', w).encode('ASCII', 'ignore')
-        words[i] = w.lower()
-    return words
-
-# function to tokenize text, and put words back to their roots
-def tokenize(text):
-
-    text = ' '.join(text)
-    tokens = PunktWordTokenizer().tokenize(text)
-
-    # lemmatize words. try both noun and verb lemmatizations
-    lmtzr = WordNetLemmatizer()
-    for i in range(0,len(tokens)):
-        #tokens[i] = tokens[i].strip("'")
-        if lang != 'english':
-            if tokens[i] in lang_dictionary:
-                tokens[i] = lang_dictionary[tokens[i]]
-        else:
-            res = lmtzr.lemmatize(tokens[i])
-            if res == tokens[i]:
-                tokens[i] = lmtzr.lemmatize(tokens[i], 'v')
-            else:
-                tokens[i] = res
+def lemmatize(text): # updated
+    for i in range(0,len(text)):
+        if text[i] in lang_dictionary:
+            text[i] = lang_dictionary[text[i]]
     
     # don't return any single letters
-    tokens = [t for t in tokens if len(t) > 1 and not t.isdigit()]
-    return tokens
+    text = [t for t in text if len(t) > 1]
+    return text
 
-def remove_stopwords(text):
-
+def removeStopwords(text, filePath):
     # remove punctuation
-    chars = ['.', '/', "'", '"', '?', '!', '#', '$', '%', '^', '&',
+    chars = ['.', '/', "'", '"', '„',  '?', '!', '#', '$', '%', '^', '&',
             '*', '(', ')', ' - ', '_', '+' ,'=', '@', ':', '\\', ',',
-            ';', '~', '`', '<', '>', '|', '[', ']', '{', '}', '–', '“',
+            ';', '~', '`', '´', '<', '>', '|', '[', ']', '{', '}', '–', '“',
             '»', '«', '°', '’']
     for c in chars:
         text = text.replace(c, ' ')
     
     text = text.split()
 
-    import nltk
-    if lang == 'english':
-        stopwords = nltk.corpus.stopwords.words('english')
-    else:
-        stopwords = open(lang + '_stopwords.txt', 'r').read().split()
-    content = [w for w in text if w.lower().strip() not in stopwords]
+    stopwords = []
+    f = open(filePath, 'r')
+    for line in f:
+        if len(line) == 0 or line[0] == '#':
+            continue
+        stopwords.append(line.split()[0])
+    content = [w for w in text if w not in stopwords]
     return content
 
 # __main__ execution
 
-import sys, re, math, unicodedata
+import math
 from optparse import OptionParser
 
 parser = OptionParser(usage='usage: %prog [options] input_file')
-parser.add_option('-l', '--language', dest='language',
-        help='language to use in tokenizing and lemmatizing. supported\
-                languages: {english, french}', metavar='LANGUAGE')
 parser.add_option('-k', '--top-k', dest='top_k',
         help='output only terms with score no less k')
 parser.add_option('-m', '--mode', dest='mode',
         help='display mode. can be either "both" or "term"')
 (options, args) = parser.parse_args()
 
-if options.language:
-    if options.language not in supported_langs:
-        print 'only ', supported_langs, ' are supported in this version.'
-        quit()
-    if options.language != 'english':
-        lang = options.language
-        loadLanguageLemmas(options.language + '_lemmas.txt')
+loadLemmata(lemmaPath)
+
 if options.top_k:
     top_k = int(options.top_k)
 display_mode = 'both'
@@ -137,20 +110,17 @@ all_files = reader.read().splitlines()
 
 num_docs  = len(all_files)
 
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.tokenize.punkt import PunktWordTokenizer
-
 print('initializing..')
 for f in all_files:
     
     # local term frequency map
     terms_in_doc = {}
     
-    doc_words    = open(f).read().lower()
+    doc_words    = open(f).read()
     #print 'words:\n', doc_words
-    doc_words    = remove_stopwords(doc_words)
+    doc_words    = removeStopwords(doc_words, stopwordPath)
     #print 'after stopwords:\n', doc_words
-    doc_words    = tokenize(doc_words)
+    doc_words    = lemmatize(doc_words)
     #print 'after tokenize:\n', doc_words
 
     #quit()
